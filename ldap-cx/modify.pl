141916e454f570000d4f26ff5f8550acd54a32aa
gjm <gjm>
2004-12-06 12:14:15 +0000

    Limited working version.

--- ldap-cx/modify.pl
+++ ldap-cx/modify.pl
@@ -2,6 +2,12 @@
 
 :- unit(ldap_modify(DN, ATTRS)).
 
+/*
+ * ATTENTION: For each attribute specified in mods, modify will replace
+ *            ALL existing attributes to the supplied value.
+ *
+ */
+ 
 
 %%---------------------------------------------------------------------------
 %% Modifies an object in an LDAP repository.
@@ -9,11 +15,16 @@
 
 item :- modify.
 
-modify.
+modify :-
+        ldx(LDX),
+        ldap_modify(LDX, DN, ATTRS).
 
 
 %---------------------------------------------------------------------------
 % $Log$
+% Revision 1.2  2004/12/06 12:14:15  gjm
+% Limited working version.
+%
 % Revision 1.1  2004/11/18 15:56:14  gjm
 % Initial revision.
 %
